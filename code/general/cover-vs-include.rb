295a3e704beb89751c1dcf7dfdbbb954a1bc87c9
ParadoxV5 <paradox.ver5@gmail.com>
2020-06-23 18:54:16 -0600

    Reworked cover-vs-include.rb to follow formats
    Range#member? is an alias of Range#include?

--- code/general/cover-vs-include.rb
+++ code/general/cover-vs-include.rb
@@ -1,15 +1,22 @@
-require "benchmark/ips"
-require "date"
-
+require 'date'
 BEGIN_OF_JULY = Date.new(2015, 7, 1)
 END_OF_JULY = Date.new(2015, 7, 31)
 DAY_IN_JULY = Date.new(2015, 7, 15)
 
-Benchmark.ips do |x|
-  x.report('range#cover?') { (BEGIN_OF_JULY..END_OF_JULY).cover? DAY_IN_JULY }
-  x.report('range#include?') { (BEGIN_OF_JULY..END_OF_JULY).include? DAY_IN_JULY }
-  x.report('range#member?') { (BEGIN_OF_JULY..END_OF_JULY).member? DAY_IN_JULY }
-  x.report('plain compare') { BEGIN_OF_JULY < DAY_IN_JULY && DAY_IN_JULY < END_OF_JULY }
+def fast
+  BEGIN_OF_JULY < DAY_IN_JULY && DAY_IN_JULY < END_OF_JULY
+end
+def avg
+  (BEGIN_OF_JULY..END_OF_JULY).cover? DAY_IN_JULY
+end
+def slowest
+  (BEGIN_OF_JULY..END_OF_JULY).include? DAY_IN_JULY
+end
 
-  x.compare!
+require 'benchmark/ips'
+Benchmark.ips do |bm|
+  bm.report('Range#cover?') {avg}
+  bm.report('Range#include?') {slowest}
+  bm.report('Plain compare') {fast}
+  bm.compare!
 end
